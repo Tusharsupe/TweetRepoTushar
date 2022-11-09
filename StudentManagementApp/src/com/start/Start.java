040912b458e66138012b5b6147797e0d3bfafea1
unknown <cogjava4355@iiht.tech>
2022-11-09 14:24:39 +0530

    added student management app for jdbc

---
+++ StudentManagementApp/src/com/start/Start.java
@@ -0,0 +1,36 @@
+package com.start;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Start {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("hello world");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			System.out.println("press 1 to add a student");
+			System.out.println("press 2 to delete a student");
+			System.out.println("press 3 to display a student");
+			System.out.println("press 4 to exit a student");
+			int choice = Integer.parseInt(br.readLine());
+
+			if (choice == 1) {
+				//add student
+			}
+			else if (choice == 2) {
+				//delete student
+			}
+			else if (choice == 3) {
+				//display student
+			}
+			else if (choice == 4) {
+				//exit menu
+			}else {
+				
+			}
+			System.out.println("Thanks for using student management system ");
+		}
+	}
+}
---
+++ StudentManagementApp/src/com/start/student/Student.java
@@ -0,0 +1,26 @@
+package com.start.student;
+
+public class Student {
+
+	private int studentId;
+	private String studentName;
+	private String studentPhone;
+	private String studentCity;
+	public Student(int studentId, String studentName, String studentPhone, String studentCity) {
+		super();
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.studentPhone = studentPhone;
+		this.studentCity = studentCity;
+	}
+	public Student(String studentName, String studentPhone, String studentCity) {
+		super();
+		this.studentName = studentName;
+		this.studentPhone = studentPhone;
+		this.studentCity = studentCity;
+	}
+	
+	
+	
+	
+}
